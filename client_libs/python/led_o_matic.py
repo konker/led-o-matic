7abd11f76e4889379657e79f88f9a788c10a6492
Konrad Markus <konker@iki.fi>
2018-06-10 16:29:57 +0300

    Rename python client library module

---
+++ client_libs/python/led_o_matic.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""
+Python library for interacting with the led-o-maticd daemon
+Author: Konrad Markus <konker@luxvelocitas.com>
+"""
+import socket
+
+class LedOMatic(object):
+    def __init__(self, port, ip='127.0.0.1'):
+        self.port = port
+        self.ip = ip
+
+        self.sock = socket.socket(socket.AF_INET, # Internet
+                                  socket.SOCK_DGRAM) # UDP
+
+
+    def _write_cmd(self, cmd):
+        self.sock.sendto(cmd.encode(), (self.ip, self.port))
+
+
+    def on(self):
+        self._write_cmd("on\n")
+
+
+    def off(self):
+        self._write_cmd("off\n")
+
+
+    def start(self, segment):
+        self._write_cmd("start:%s\n" % segment)
+
+
+    def stop(self, segment):
+        self._write_cmd("stop:%s\n" % segment)
+
+
+    def reverse(self, segment):
+        self._write_cmd("reverse:%s\n" % segment)
+
+
+    def clear(self, segment):
+        self._write_cmd("clear:%s\n" % segment)
+
+
+    def text(self, segment, text):
+        self._write_cmd("text:%s:%s\n" % (segment, text))
+
+
+
+if __name__ == '__main__':
+    server = LedOMatic(7890)
+    server.on()
+    server.start(0)
+    server.text(0, "KONKER IS KING!!")
+
