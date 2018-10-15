52f4674b96f97b35ac8e9c3a4f51c87dc71f38c1
Konrad Markus <konker@iki.fi>
2018-10-15 21:41:46 +0300

    Tidy up booking_counts.py script

--- client_libs/python/booking_counts.py
+++ client_libs/python/booking_counts.py
@@ -2,24 +2,11 @@
 import time
 import requests
 
-from gtts import gTTS
-from pygame import mixer
-
 from led_o_matic import LedOMatic
 
 DATA_DELAY_SECS = 10
 ANIM_DELAY_SECS = 0.5
 STAR = '.'
-TARGET = '9000000'
-
-booking_milestones = {
-    999500: "Onwards.",
-    999600: "Looking great!",
-    999700: "My word, this is cool!",
-    999800: "I think I shall have a cup of tea",
-    999900: "By george, we are almost there!",
-    999980: "Gather round, grab the bubbly.",
-}
 
 
 # Set up URL and headers
@@ -27,7 +14,7 @@ url = 'https://counter.analytics.maas.global/current'
 headers = {'x-api-key': 'fWFo8rlFYm849XgetJMMp1q1sgcA10TU7UerCqlN'}
 
 # Connect to sign
-lomd = LedOMatic(ip='10.0.1.52', port=7890)
+lomd = LedOMatic(ip='0.0.0.0', port=7890)
 
 
 def main():
@@ -52,32 +39,6 @@ def main():
                 animate_number_change(last_bookings_disp, bookings_disp)
                 lomd.center(0)
 
-                """
-                milestones = booking_milestones.keys()
-
-                for milestone in sorted(milestones, reverse=True):
-                    if int(bookings) >= milestone and int(last_bookings) < milestone:
-                        difference = int(TARGET) - milestone
-
-                        # Generate text to speech phrase
-                        text = booking_milestones[milestone] + "..." + "Less than " + str(difference) + " bookings to go!"
-                        tts = gTTS(text=text, lang="en")
-                        tts.save("audio.mp3")
-
-                        # Playback TTS audio
-                        mixer.init()
-                        mixer.music.load("audio.mp3")
-                        mixer.music.play()
-                        
-                        break
-
-                if (int(bookings) >= int(TARGET) and int(last_bookings) < int(TARGET)):
-                    animate_flashing(24)
-                    mixer.init()
-                    mixer.music.load("HeavenlyMotion.mp3")
-                    mixer.music.play()
-
-                """
             last_bookings = bookings
             last_bookings_disp = bookings_disp
             time.sleep(DATA_DELAY_SECS)
@@ -102,9 +63,6 @@ def animate_number_change(from_n, to_n):
     from_c = list(from_n)
     to_c = list(to_n)
 
-    #print(from_c)
-    #print(to_c)
-
     to_c_anim = to_c.copy()
 
     # Change all different digits to a * character
@@ -116,7 +74,6 @@ def animate_number_change(from_n, to_n):
 
     while STAR in to_c_anim:
         s = "".join(to_c_anim)
-        #print(s)
         lomd.text(0, s)
         lomd.center(0)
 
@@ -125,8 +82,6 @@ def animate_number_change(from_n, to_n):
         time.sleep(ANIM_DELAY_SECS)
 
     s = "".join(to_c_anim)
-    #print(s)
-    #print('------')
     lomd.text(0, s)
     lomd.center(0)
 
